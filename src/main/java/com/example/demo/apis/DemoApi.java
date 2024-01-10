7dcf3e98f1aee97473517ebe43d32e01063f1f2c
email <Vemula.RamaDevi@lumen.com>
2024-01-10 14:04:40 +0530

    initial commit

---
+++ src/main/java/com/example/demo/apis/DemoApi.java
@@ -0,0 +1,17 @@
+package com.example.demo.apis;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class DemoApi {
+
+    @GetMapping
+    public ResponseEntity<String> get()
+    {
+        return ResponseEntity.ok("hi im rama");
+    }
+}
